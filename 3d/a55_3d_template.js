5cf10fa34934fa6492b117f131f251406b68f55b
neodigm <3151842+neodigm@users.noreply.github.com>
2022-08-26 19:55:27 -0500

    interactive_infographics

--- 3d/a55_3d_template.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from 'three'
-
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-
-window.o3 = THREE;
-window.o3loadr = GLTFLoader;
-window.o3orbit = OrbitControls;
